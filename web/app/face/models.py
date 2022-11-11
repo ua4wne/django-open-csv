e3d9fe953a2cf2aa785b026e555c5e3b39fa0a27
dervish <rai68@yandex.ru>
2022-11-11 19:55:35 +0300

    finished face recognition

--- web/app/face/models.py
+++ web/app/face/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 class Url(models.Model):
     image_url = models.CharField(max_length=200)
-    
+
+
+class BoundingBox(models.Model):
+    top = models.DecimalField(decimal_places=3, max_digits=5)
+    right = models.DecimalField(decimal_places=3, max_digits=5)
+    bottom = models.DecimalField(decimal_places=3, max_digits=5)
+    left = models.DecimalField(decimal_places=3, max_digits=5)
+    image = models.ForeignKey(Url, on_delete=models.CASCADE)
--- web/app/face/urls.py
+++ web/app/face/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('delete/<url_id>', views.delete, name='delete'),
 ]
